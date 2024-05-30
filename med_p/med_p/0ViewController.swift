4b0525ed1758f61083d68742dfed21a12e5ce800
Sokolova Valeria <114908310+lkjhgfff@users.noreply.github.com>
2024-05-30 13:36:45 -0700

    Add files via upload

---
+++ med_p/med_p/0ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  0ViewController.swift
+//  med_p
+//
+//  Created by Valeria on 28/05/2024.
+//  Copyright © 2024 org. All rights reserved.
+//
+
+import UIKit
+
+class _ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ med_p/med_p/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  med_p
+//
+//  Created by Valeria on 27/05/2024.
+//  Copyright © 2024 org. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //override func viewDidAppear(_ animated:Bool) {
+    //    super.viewDidAppear(animated)
+      
+    //   let player = AVPlayer(url:  URL(fileURLWithPath: Bundle.main.path(forResource: "video", ofType: "mp4")!))
+    //    let layer = AVPlayerLayer(player:player)
+    //    layer.frame = view.bounds
+    //    view.layer.addSublayer(layer)
+        //player.play()
+    //}
+
+}
+
