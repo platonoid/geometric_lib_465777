4fa8d5a0466a5347e65a23b01ed1866bda26ae26
Igor1128 <464927@niuitmo.ru>
2024-09-29 18:57:08 +0300

    The formula for calculating the area of a triangle has been fixed, comments have been added

--- triangle.py
+++ triangle.py
@@ -1,6 +1,11 @@
+import math
+
 def area(a, b, c):
-    return (a + b + c) / 2
+    '''Принимает числа a,b,c, считает число p, возвращает площадь треугольника'''
+    p = (a+b+c)/2
+    return math.sqrt(p * (p-a) * (p-b) * (p-c))
 
 
 def perimeter(a, b, c):
+    '''Принимает числа a,b,c и возвращает периметр треугольника'''
     return a + b + c
