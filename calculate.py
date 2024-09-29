092076fa3df01ebfb040452295ba297bc7f866ce
Igor1128 <464927@niuitmo.ru>
2024-09-29 19:01:18 +0300

    Comments have been added, added import triangle

--- calculate.py
+++ calculate.py
@@ -1,14 +1,32 @@
 import circle
 import square
+import triangle
 
 
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 sizes = {}
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+	"""
+	Вычисляет периметр или площадь заданной фигуры.
+
+	Args:
+	fig (str): название фигуры ("круг" или "квадрат" или "треугольник").
+	func (str): функция для вычисления ("периметр" или "площадь").
+	size (list):список размеров фигуры. 
+				Для круга он содержит радиус.
+				Для квадрата он содержит длину его стороны.
+				Для треугольника он содержит длины трех его строн.
+
+	Returns:
+	Выводит результат на консоль.
+
+	"""
+	assert fig in figs 
+	'''Недопустимое название фигуры'''
+	assert func in funcs 
+	'''Недопустимое название функции'''
 
 	result = eval(f'{fig}.{func}(*{size})')
 	print(f'{func} of {fig} is {result}')
