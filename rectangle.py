f31c51ba3cb6a41e6d1a19d2420e39ae773b3054
jisqwe <408273@niuitmo.ru>
2024-09-30 15:09:44 +0300

    add documentation

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,17 @@
-def area(a, b): 
+def area(a, b):
+    ''' Find the area of rectangle
+            Parameters:
+                a (double): first side
+                b (double): second side
+            Returned values:
+                rec_area (double): area of the rectangle '''
     return a * b 
 
-def perimeter(a, b): 
+def perimeter(a, b):
+    ''' Find the perimeter of rectangle
+            Parameters:
+                a (double): first side
+                b (double): second side
+            Returned values:
+                rec_perimeter (double): perimeter of the rectangle '''
     return (a + b) * 2
