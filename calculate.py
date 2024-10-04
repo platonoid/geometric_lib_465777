6b6dc94587d7c3c684d42ec538304cefd7d8498e
Artem Iushin <artemiushin@mail.ru>
2024-10-04 15:52:42 +0300

    update calculate.py

--- calculate.py
+++ calculate.py
@@ -2,32 +2,31 @@ import circle
 import square
 
 
-figs = ['circle', 'square']
+figs = ['circle', 'square'] 
 funcs = ['perimeter', 'area']
 sizes = {}
 
-def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+def calc(fig, func, size): '''Функция принимает параметры: фигуру (fig), funcфункцию для вычисления(func) и размер фигуры(size) '''
+ assert fig in figs''' проверяет , есть ли fig в figs'''
+ assert func in funcs '''проверяет, есть ли func в funcs'''
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+ result = eval(f'{fig}.{func}(*{size})')'''f-строка, которая позволяет вставлять значения переменных в строку.      
+ print(f'{func} of {fig} is {result}') '''Выводится строка с результатом вычисления'''
 
-if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
-
-
-
+if name == "main": '''Проверка, является ли модуль главным'''
+''' задача переменных'''
+ func = ''
+ fig = ''
+ size = list()
+'''Цикл ввода фигуры. Вводить надо корректное значение из figs'''
+ while fig not in figs:
+  fig = input(f"Enter figure name, avaliable are {figs}:\n")
+'''Цикл ввода фигуры. Вводить надо корректное значение из funcs'''
+ while func not in funcs:
+  func = input(f"Enter function name, avaliable are {funcs}:\n")
+'''Цикл ввода размера для фигуры. Len(size) должен совпадать с количеством аргументов fig и func'''
+ while len(size) != sizes.get(f"{func}-{fig}", 1): '''получение кол-во аргументов из sizes. Если такого нет то возвращается 1'''
+'''Ввод размеров через пробел. Возвращается список строк'''
+  size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+ 
+ calc(fig, func, size) '''Вызов функции'''
