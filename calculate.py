d4c7d665734519efca785fd95df12d2bcf0bb737
Daniel <nedanielmalahov@yahoo.com>
2024-10-05 03:14:30 +0300

    all python files have got documentation

--- calculate.py
+++ calculate.py
@@ -1,33 +1,38 @@
 import circle
 import square
 
-
+# Доступные фигуры и функции
 figs = ['circle', 'square']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {}  # Словарь для хранения количества необходимых параметров для фигур и функций
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+    # Проверка корректности введенных данных
+    assert fig in figs
+    assert func in funcs
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    # Динамический вызов функции с использованием eval
+    result = eval(f'{fig}.{func}(*{size})')
+    print(f'{func} of {fig} is {result}')
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
+    func = ''
+    fig = ''
+    size = list()
     
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
+    # Запрос фигуры у пользователя
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}:\n")
+    
+    # Запрос функции у пользователя
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}:\n")
+    
+    # Запрос размеров фигуры у пользователя
+    while len(size) != sizes.get(f"{func}-{fig}", 1):
+        size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+    
+
 
 
 
